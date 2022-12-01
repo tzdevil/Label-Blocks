2ad391e66db603dd3396c6f6aa6568d0d4956b3d
tzdevil <79012136+tzdevil@users.noreply.github.com>
2022-12-01 15:48:18 +0300

    Label Blocks 1.1.0

--- Editor/CreateLabelBlocks.cs
+++ Editor/CreateLabelBlocks.cs
@@ -1,3 +1,4 @@
+#if (UNITY_EDITOR)
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
@@ -80,3 +81,4 @@ namespace tzdevil.LabelBlocks
         }
     }
 }
+#endif
--- Editor/LabelBlocks.cs
+++ Editor/LabelBlocks.cs
@@ -1,3 +1,4 @@
+#if (UNITY_EDITOR)
 using UnityEditor;
 using UnityEngine;
 
@@ -43,3 +44,4 @@ namespace tzdevil.LabelBlocks
         }
     }
 }
+#endif
